4e690a7db605d170d2576bcec1b0383cc59bcb76
Abner Andrade <aa.abnerandrade@outlook.com.br>
2025-04-23 01:57:53 -0300

    builded teste i

---
+++ test.js
@@ -0,0 +1,4 @@
+// Teste 1: Verifica se o formulário existe no DOM
+const form = document.querySelector("form");
+console.assert(form !== null, "✅ Teste 1 passou: Formulário encontrado com sucesso!");
+
